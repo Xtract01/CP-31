5e0e23592a6a55778327077ed435b121f506eb16
Xtract01 <meena.balwir149@gmail.com>
2026-01-17 03:25:20 +0530

    Rated-900 Q-7

---
+++ Rated-900/D_Balanced_Round.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h> // Includes almost all standard headers
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define endl '\n'
+
+
+const ll INF = LLONG_MAX;        
+const int INF_INT = INT_MAX;
+const ll NEG_INF = LLONG_MIN;
+const int MOD = 1e9 + 7;        
+const double EPS = 1e-9;
+const double PI = acos(-1.0);
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+       ll n,k;
+       cin>>n>>k;
+       ll maxconsecValid= 1;
+       vector<ll> arr(n);
+       for(ll i=0 ;i<n ;i++){
+       cin>>arr[i];
+       }
+       sort(arr.begin(),arr.end());
+       ll consecValid=1;
+       for(ll i=1 ; i<n ; i++){
+        if(arr[i]-arr[i-1]<=k) consecValid++;
+        else consecValid=1;
+        maxconsecValid = max(consecValid , maxconsecValid);
+       }
+       cout<< n-maxconsecValid<<endl;
+    }
+    return 0;
+}
