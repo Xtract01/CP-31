f5699151f724abb65f08d275144babf1884cfb3d
Xtract01 <meena.balwir149@gmail.com>
2026-03-10 01:59:34 +0530

    Rated-900 Q-17

--- Rated-900/B_Array_Cloning_Technique.cpp
+++ Rated-900/B_Array_Cloning_Technique.cpp
@@ -14,32 +14,47 @@ using namespace std;
 #define ff first
 #define ss second
 #define endl '\n'
-const ll INF = LLONG_MAX;
+
+
+const ll INF = LLONG_MAX;        
 const int INF_INT = INT_MAX;
 const ll NEG_INF = LLONG_MIN;
 const int MOD = 1e9 + 7;        
 const double EPS = 1e-9;
 const double PI = acos(-1.0);
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
+
     int t;
     cin >> t;
     while (t--) {
-       int n;
-       cin>>n;
-       string s;
-       cin>>s;
-       int maxSub = 1;
-       int currSub = 1;
-       for(int i=1 ; i<s.length();i++){
-        if(s[i]==s[i-1]){
-            currSub++;
-            maxSub = max(maxSub,currSub);
+        int n;
+        cin>>n;
+        map<ll,ll> mp;
+        for(int i=0 ; i<n ;i++){
+            ll x ;
+            cin>>x;
+            mp[x]++;
         }
-        else currSub = 1;
-       }
-       cout<<maxSub+1<<endl;
+        ll current_highest = 0;
+        for(auto it:mp){
+            current_highest = max(current_highest,it.second);
+        }
+        ll ops =0 ;
+        while(current_highest < n){
+            ops++;
+            if(current_highest*2 <= n){
+                ops += current_highest;
+                current_highest *= 2;
+            }
+            else {
+                ops += (n-current_highest);
+                current_highest = n;
+            }
+        }
+        cout<<ops<<endl;
     }
     return 0;
 }
