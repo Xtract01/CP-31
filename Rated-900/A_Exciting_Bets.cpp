a3f6117f72769a1867403b4a489f02b606b46a30
Xtract01 <meena.balwir149@gmail.com>
2026-03-30 15:47:03 +0530

    Rated-900 Q-24

---
+++ Rated-900/A_Exciting_Bets.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h> // Includes almost all standard headers
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define endl '\n'
+
+
+const ll INF = LLONG_MAX;        
+const int INF_INT = INT_MAX;
+const ll NEG_INF = LLONG_MIN;
+const int MOD = 1e9 + 7;        
+const double EPS = 1e-9;
+const double PI = acos(-1.0);
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        ll a ,b ;
+        cin>>a>>b;
+        if(b>a){
+            swap(b,a);
+        }
+        if(a==b) cout<<0<<" "<<0<<endl;
+        else {
+            ll gcd = a-b;
+            ll ans = min(b%gcd,gcd-b%gcd);
+            cout<<gcd<<" "<<ans<<endl;
+        }
+    }
+    return 0;
+}
