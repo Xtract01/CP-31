60dadcc971a3b88b09beab8d6e47265dee582425
Xtract01 <meena.balwir149@gmail.com>
2026-03-30 14:47:39 +0530

    Rated -900 Q-22,23

---
+++ Rated-900/B_Luntik_and_Subsequences.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h> // Includes almost all standard headers
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define endl '\n'
+
+
+const ll INF = LLONG_MAX;        
+const int INF_INT = INT_MAX;
+const ll NEG_INF = LLONG_MIN;
+const int MOD = 1e9 + 7;        
+const double EPS = 1e-9;
+const double PI = acos(-1.0);
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        // Solve each test case here
+        ll n;
+        cin>>n;
+        ll cnt0 = 0;
+        ll cnt1 = 0;
+        for(ll i=0 ; i<n ; i++){
+            ll x;
+            cin>>x;
+            if(x==1) cnt1++;
+            else if(x==0) cnt0++;
+        }
+        cout << cnt1 * (1LL << cnt0) << endl;
+    }
+    return 0;
+}
