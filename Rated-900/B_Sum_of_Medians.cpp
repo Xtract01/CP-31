e37be5452f6349b20932ede33225069a6f0a0877
Xtract01 <meena.balwir149@gmail.com>
2026-04-01 02:03:21 +0530

    Rated-900 Q-31,28

---
+++ Rated-900/B_Sum_of_Medians.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h> // Includes almost all standard headers
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define endl '\n'
+
+
+const ll INF = LLONG_MAX;        
+const int INF_INT = INT_MAX;
+const ll NEG_INF = LLONG_MIN;
+const int MOD = 1e9 + 7;        
+const double EPS = 1e-9;
+const double PI = acos(-1.0);
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        // Solve each test case here
+        ll n ,k;
+        cin>>n>>k;
+        ll x = n*k;
+        vector<ll>a(x);
+        for(int i=0 ;i<x ;i++){
+            cin>>a[i];
+        }
+        ll pointer = n*k;
+        ll sum = 0;
+        while(k--){
+            pointer-=(n/2+1);
+            sum+=a[pointer];
+        }
+        cout<<sum<<endl;
+    }
+    return 0;
+}
