48a8e871916ad7a45b17243ae8b66a18cd17bc4e
Xtract01 <meena.balwir149@gmail.com>
2026-04-27 03:12:36 +0530

    Rated-1000 Q-5

---
+++ Rated-1000/B_Monsters.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h> // Includes almost all standard headers
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define endl '\n'
+
+
+const ll INF = LLONG_MAX;        
+const int INF_INT = INT_MAX;
+const ll NEG_INF = LLONG_MIN;
+const int MOD = 1e9 + 7;        
+const double EPS = 1e-9;
+const double PI = acos(-1.0);
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        ll n , k;
+        cin>>n>>k;
+        
+        vector<pair<ll,ll>> arr(n);
+        for(ll i = 0 ; i<n ; i++){
+            ll x;
+            cin>>x;
+            arr[i] = {x,i};
+        }
+        sort(arr.begin(), arr.end(), [&](pair<ll,ll> a, pair<ll,ll> b){
+            ll ra = a.first % k;
+            ll rb = b.first % k;
+
+            if(ra == 0) ra = k;
+            if(rb == 0) rb = k;
+
+            if(ra == rb) return a.second < b.second;
+            return ra > rb;
+        });
+        for(auto &p:arr){
+            cout<<p.second+1<<" ";
+
+        }
+        cout<<endl;
+
+    }
+    return 0;
+}
