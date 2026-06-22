4ab47b7f68a78d0298452776490fca60845df9d5
Xtract01 <meena.balwir149@gmail.com>
2026-06-22 19:28:13 +0530

    Rated-1100 Q-1,2

---
+++ Rated-1100/C_Quests.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h> // Includes almost all standard headers
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define endl '\n'
+
+
+const ll INF = LLONG_MAX;        
+const int INF_INT = INT_MAX;
+const ll NEG_INF = LLONG_MIN;
+const int MOD = 1e9 + 7;        
+const double EPS = 1e-9;
+const double PI = acos(-1.0);
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        ll n,k;
+        cin>>n>>k;
+        vector<ll> a(n),b(n);
+        for(ll i=0 ; i<n ; i++) cin>>a[i];
+        for(ll i=0 ; i<n ; i++) cin>>b[i];
+
+        ll maxi = 0 , sum = 0 , ans = 0;
+        for(ll i=0 ; i< min(n,k);i++){
+            sum+=a[i];
+            maxi = max(maxi,b[i]);
+            ans = max(ans,sum+(k-i-1)*maxi);
+        }
+        cout<<ans<<endl;
+    }
+    return 0;
+}
