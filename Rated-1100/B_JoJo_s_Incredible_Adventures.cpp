0e5293c495c4ef462b48cd203b106b1ea262a5be
Xtract01 <meena.balwir149@gmail.com>
2026-07-04 15:01:15 +0530

    Rated-1100 Q-14,15

---
+++ Rated-1100/B_JoJo_s_Incredible_Adventures.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h> // Includes almost all standard headers
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define endl '\n'
+
+
+const ll INF = LLONG_MAX;        
+const int INF_INT = INT_MAX;
+const ll NEG_INF = LLONG_MIN;
+const int MOD = 1e9 + 7;        
+const double EPS = 1e-9;
+const double PI = acos(-1.0);
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin>>s;
+        ll cnt = 0;
+        ll curr = 0;
+        int n = s.size();
+         if (count(s.begin(), s.end(), '1') == n) {
+            cout << 1LL * n * n << '\n';
+            continue;
+        }
+
+        string t = s + s;
+
+        ll mx = 0, cur = 0;
+        for (char c : t) {
+            if (c == '1') {
+                cur++;
+                mx = max(mx, cur);
+            } else {
+                cur = 0;
+            }
+        }
+
+        mx = min(mx, (ll)n);
+
+        cout << ((mx + 1) / 2) * ((mx + 2) / 2) << '\n';
+    }
+    return 0;
+}
