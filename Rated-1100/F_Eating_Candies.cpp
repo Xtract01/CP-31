be22b79d3a1872092c8fa70cb4186fde96f50fc0
Xtract01 <meena.balwir149@gmail.com>
2026-07-07 15:01:29 +0530

    Rated-1100 Q-26,27,28,29

---
+++ Rated-1100/F_Eating_Candies.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define endl '\n'
+
+const ll INF = LLONG_MAX;
+const int INF_INT = INT_MAX;
+const ll NEG_INF = LLONG_MIN;
+const int MOD = 1e9 + 7;
+const double EPS = 1e-9;
+const double PI = acos(-1.0);
+
+void solve() {
+    // Write your solution for one test case here
+    ll n;
+    cin>>n;
+    vector<ll> a(n);
+    for(ll i=0;i<n;i++){
+        cin>>a[i];
+    }
+    ll l=0 , r = n-1;
+    ll left = 0 , right=0;
+    ll ans = 0;
+    while(l<=r){
+        if(left<=right){
+            left+=a[l++];
+        }
+        else right += a[r--];
+        if(left==right){
+            ans = l + (n-1-r);
+        }
+    }
+    cout<<ans<<endl;
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
